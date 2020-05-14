1710ebe106c21caad1b31d6a09f7001ac74b15f9
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-05-14 16:05:41 +0300

    Работа над ошибками №1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-let page = document.querySelector('.page');
-let popup = page.querySelector('.display-none');
-let editButton = page.querySelector('.profile__edit');
-let closeButton = popup.querySelector('.popup__close');
-let profileName = page.querySelector('.profile__name');
-let profileStatus = page.querySelector('.profile__status');
-let infoSaveChange = page.querySelector('.popup__info-save');
-let nameChange = popup.querySelector('.popup__name-change');
-let statusChange = popup.querySelector('.popup__status-change');
+const page = document.querySelector('.page');
+const popup = page.querySelector('.popup_none');
+const editButton = page.querySelector('.profile__edit');
+const closeButton = popup.querySelector('.popup__close');
+const profileName = page.querySelector('.profile__name');
+const profileStatus = page.querySelector('.profile__status');
+const infoSaveChange = page.querySelector('.popup__info-save');
+const nameChange = popup.querySelector('.popup__name-change');
+const statusChange = popup.querySelector('.popup__status-change');
 
 function openPopup() {
-  popup.classList.remove('display-none');
+  popup.classList.remove('popup_none');
   nameChange.value = profileName.textContent;
   statusChange.value = profileStatus.textContent;
 }
 
 function closePopup() {
-  popup.classList.add('display-none');
+  popup.classList.add('popup_none');
 }
 
+window.onkeydown = function(event) {
+  if (event.keyCode == 27) {
+    closePopup();
+  }
+};
+
 function saveChanges() {
-  profileName.textContent = `${nameChange.value}`;
-  profileStatus.textContent = `${statusChange.value}`;
+  profileName.textContent = nameChange.value;
+  profileStatus.textContent = statusChange.value;
 
   nameChange.value = '';
   statusChange.value = '';
