e70fa879bb13edc829be254d3595a106bbe295a5
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-06-01 21:49:09 +0300

    5.Работа над ошибками №1

--- js/script.js
+++ js/script.js
@@ -1,46 +1,3 @@
-const page = document.querySelector('.page');
-const popup = document.querySelector('.popup')
-// Попап для смены имени
-const popupEdit = page.querySelector('.popup__edit');
-const editButton = page.querySelector('.profile__edit');
-const infoSaveChange = popupEdit.querySelector('.popup__info-change');
-const nameChange = popupEdit.querySelector('.popup__name-change');
-const profileName = page.querySelector('.profile__name');
-const profileStatus = page.querySelector('.profile__status');
-const closeButton = page.querySelector('.popup__close');
-const statusChange = popupEdit.querySelector('.popup__status-change');
-
-
-function openPopup(blockPop) {      //Открывает попапы
-  blockPop.classList.add('popup_active');
-  blockPop.querySelector('.popup__close').addEventListener('click', () => closePopup(blockPop));
-}
-
-function closePopup(blockPop) {     //Закрывает попапы
-  blockPop.classList.remove('popup_active')
-}
-
-function openPopupEdit() {
-  openPopup(popupEdit)
-  nameChange.value = profileName.textContent;
-  statusChange.value = profileStatus.textContent;
-};
-
-
-function saveChanges(event) {
-  event.preventDefault()
-  profileName.textContent = nameChange.value;
-  profileStatus.textContent = statusChange.value;
-  
-  nameChange.value = '';
-  statusChange.value = '';
-  closePopup(popupEdit);
-};
-
-editButton.addEventListener('click', openPopupEdit);
-infoSaveChange.addEventListener('submit', saveChanges);
-
-
 // Массив первых шести фотокарточек
 const initialCards = [ 
   {
@@ -68,72 +25,116 @@ const initialCards = [
     link: 'https://images.unsplash.com/photo-1542649761-0bdd3753c8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
   },
 ];
-// Загрузка карточек из массива
-const sectionCards = document.querySelector('.cards');
-const cardTemplate = document.querySelector('#new-card').content;     // Темплейт фотокарточки
 
+// Все объявленные константы
+const page = document.querySelector('.page');
+// Попап для смены имени
+const popupEdit = page.querySelector('.popup__edit');                       // Выбор попапа с формой изменения имени и статуса
+const editButton = page.querySelector('.profile__edit');                    // Выбор кнопки "карандаш"
+const infoSaveChange = popupEdit.querySelector('.popup__info-change');      // Выбор формы изменения имени и статуса
+const closeButton = page.querySelector('.popup__close');                    // Выбор кнопки закрытия
+const profileName = page.querySelector('.profile__name');                   // Выбор тега с именем
+const profileStatus = page.querySelector('.profile__status');               // Выбор тега со статусом
+const nameChange = popupEdit.querySelector('.popup__name-change');          // Выбор инпута смены имени
+const statusChange = popupEdit.querySelector('.popup__status-change');      // Выбор инпута смены статуса
+const sectionCards = document.querySelector('.cards');                      // Секция cards
+const cardTemplate = document.querySelector('#new-card').content;           // Темплейт фотокарточки
+// Попап для добавления карточек
+const popupCardAdd = page.querySelector('.popup__new-card');                // Выбор попапа с формой добавления фотокарточки
+const cardAddButton = page.querySelector('.profile__add-button');           // Выбор кнопки "плюс"
+const cardAddSave = popupCardAdd.querySelector('.popup__new-card-add');     // Выбор формы добавления фотокарточек
+const cardAddName = popupCardAdd.querySelector('.popup__new-card-name');    // Выбор инпута названия фотокарточки
+const cardAddUrl = popupCardAdd.querySelector('.popup__new-card-url');      // Выбор инпута ссылки фотокарточки
+// Попап для просмотра фото
+const lightBox = page.querySelector('.popup__lightbox');                    // Выбор попапа "lightbox"
+const lightboxImage = lightBox.querySelector('.popup__lightbox-image');     // Выбор фото в "lightbox"
+const lightboxCaption = lightBox.querySelector('.popup__lightbox-caption'); // Выбор названия в "lightbox"
+
+// Функции
+
+// Открывает попапы
+function openPopup(blockPop) {
+  blockPop.classList.add('popup_active');
+  blockPop.querySelector('.popup__close').addEventListener('click', () => closePopup(blockPop));
+}
+
+// Закрывает попапы
+function closePopup(blockPop) {
+  blockPop.classList.remove('popup_active')
+}
+
+// Открытие попапа с пустыми инпутами (форма изменения имени и статуса)
+function openPopupEdit() {
+  openPopup(popupEdit)
+  nameChange.value = profileName.textContent;
+  statusChange.value = profileStatus.textContent;
+};
+
+// Сохраняет изменения в форме изменения имени и статуса
+function saveChanges(event) {
+  event.preventDefault()
+  profileName.textContent = nameChange.value;
+  profileStatus.textContent = statusChange.value;
+  
+  nameChange.value = '';
+  statusChange.value = '';
+  closePopup(popupEdit);
+};
+
+// Функция тела фотокарточек
 function createCards(name, link) {
   const cardFromArrays = cardTemplate.cloneNode(true);
   const cardPhoto = cardFromArrays.querySelector('.card__photo')
-  cardPhoto.src = link;
   cardFromArrays.querySelector('.card__photo-name').textContent = name;
+  cardPhoto.src = link;
   cardPhoto.alt = name;
-  
-  cardFromArrays.querySelector('.card__like').addEventListener('click', function (evt) {      //Отвечает за лайк
+
+  cardFromArrays.querySelector('.card__like').addEventListener('click', function (evt) {      // Отвечает за лайк
   evt.target.classList.toggle('card__like_active');
   });
 
-  cardFromArrays.querySelector('.card__delete').addEventListener('click', function (evt) {      //Отвечает за удаление
+  cardFromArrays.querySelector('.card__delete').addEventListener('click', function (evt) {    // Отвечает за удаление
     evt.target.closest('.card').remove();
   });
 
-  cardPhoto.addEventListener('click', function () {      //Отвечает за lightBox
+  cardPhoto.addEventListener('click', function () {                                           // Отвечает за lightBox
     lightboxImage.src = link;
     lightboxCaption.textContent = name;
     openPopup(lightBox);
   });
   
-  sectionCards.prepend(cardFromArrays);
+  return cardFromArrays;
 };
 
-
+// Достаём фотокарточки из массива и вставляем в cards
 initialCards.forEach(function (item) {
-  createCards(item.name, item.link)
+  sectionCards.prepend(createCards(item.name, item.link));
 });
 
-
-
-//Попап для добавления карточек
-const popupCardAdd = page.querySelector('.popup__new-card')
-const cardAddButton = page.querySelector('.profile__add-button')
-const cardAddSave = popupCardAdd.querySelector('.popup__new-card-add')
-const cardAddName = popupCardAdd.querySelector('.popup__new-card-name');
-const cardAddUrl = popupCardAdd.querySelector('.popup__new-card-url');
-
+// Открытие попапа с пустыми инпутами (форма добавления фотокарточек)
 function openPopupCardAdd() {
   openPopup(popupCardAdd);
   cardAddName.value = '';
   cardAddUrl.value = '';
 };
 
+// Вставляем фотокарточки в cards
+function prependNewCard() {
+  sectionCards.prepend(createCards(cardAddName.value, cardAddUrl.value));
+}
 
+// Добавляет новые фотокарточки
 function addNewCard(event) {
   event.preventDefault()
-  createCards(cardAddName.value, cardAddUrl.value)
   
-  cardAddName.value = '';
-  cardAddUrl.value = '';
-  
+  createCards(cardAddName.value, cardAddUrl.value);
+
+  prependNewCard()
+
   closePopup(popupCardAdd);
 }
 
-cardAddButton.addEventListener('click', openPopupCardAdd);
-cardAddSave.addEventListener('submit', addNewCard);
-
-//Попап для просмотра фото
-const lightBox = page.querySelector('.popup__lightbox');
-const lightboxImage = lightBox.querySelector('.popup__lightbox-image');
-const lightboxCaption = lightBox.querySelector('.popup__lightbox-caption');
-
-
-
+editButton.addEventListener('click', openPopupEdit);          // Кнопка "карандаш"
+infoSaveChange.addEventListener('submit', saveChanges);       // Отправка формы изменения имени и статуса
+cardAddButton.addEventListener('click', openPopupCardAdd);    // Кнопка "плюс"
+cardAddSave.addEventListener('submit', addNewCard);           // Отправка формы добавления фотокарточки
